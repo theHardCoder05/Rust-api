2faa755ac6d64bdf2e7992f5c9b60383643090a8
HardCoder <bitlabs27@gmail.com>
2023-03-06 14:57:48 +0900

    remove the internal network in docker-compose file

--- src/main.rs
+++ src/main.rs
@@ -22,32 +22,32 @@ mod persistance;
 use cors::*;
 use handlers::*;
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
 
 #[launch]
 async fn rocket() -> _ {
     pretty_env_logger::init();
     dotenv().ok();
-    println!("Server starting....{}", "Starting");
-    // let pool = PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect_lazy(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
-    //     .expect("Failed to create Postgres connection pool!");
+    println!("Server starting....{}","Starting");
+        let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
 
-    // let answers_dao = AnswersDaoImpl::new(pool);
+
+    let answers_dao = AnswersDaoImpl::new(pool);
     println!("Started");
     rocket::build()
-    .mount(
-        "/",
-        routes![
-            hello_world,
+        .mount(
+            "/",
+            routes![
+                create_answer,
+                get_answers,
             
-        
-        ],
-    )
+            ],
+        )
         .attach(CORS)
-        // .manage(Box::new(answers_dao) as Box<dyn AnswersDao + Send + Sync>)
+        .manage(Box::new(answers_dao) as Box<dyn AnswersDao + Send + Sync>)
+
+    
 }
